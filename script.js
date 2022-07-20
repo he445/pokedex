ee8ab14b11b4d5373fed7815aa56d5d078a52539
he445 <103337053+he445@users.noreply.github.com>
2022-07-19 22:59:01 -0300

    0.02

--- script.js
+++ script.js
@@ -1,40 +1,35 @@
-//  const fetchPokemon = () => {
+  // const fetchPokemon = () => {
 async function getAllPokemons() {
-  const resp = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
-  );
+const resp = await fetch(
+   "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+ );
   const data = await resp.json();
-
-  /* console.log(data) */
-
-data.results.forEach(async function (item) {
+  data.results.forEach(async function (item) {
     const respPoke = await fetch(item.url);
-    const dataPoke = await respPoke.json();
+    const pokemon = await respPoke.json();
     let idArrey = [];
-    idArrey.push(dataPoke.id);
-    let idnum = idArrey.length;
-    const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
-    const pokemonsPromisses = [];
-    for (i = 1; i <= idnum; i++) {
-        pokemonsPromisses.push(
-         fetch(getPokemonUrl(i)).then((response) => response.json())
-       );
-      }
-      Promise.all(pokemonsPromisses).then((pokemons) => {
-       console.log(pokemons);
-     });
+    idArrey.push(pokemon.id);
+    idnum= function(){for(let i=0;i<=idArrey.length;i++){return idArrey[i]}}
+     
+   const liPkemon = document.querySelector('#father').insertAdjacentHTML('beforeend', ` <li class="line"> <h2>${pokemon.name}</h2>
+   <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${idnum()}.png"  alt=""> 
+      </li> <br/>` )
+    
+  
   });
- 
-}
-
- getAllPokemons();
 
 
+ }
 
+getAllPokemons();
 
-// const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
-//  const pokemonsPromisses = [];
  
 
+ 
+  
 
-// fetchPokemon();
+
+
+
+
+
